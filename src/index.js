8eed66ea9536b0f5258b2387616ccf39dee4d60c
wakana <wakanawardrobe@gmail.com>
2022-08-07 15:41:26 +0100

    project list toggle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+const projects = [...document.querySelectorAll('.project')];
+
+projects.forEach(project => {
+    project.addEventListener('click', (e) => {
+        projects.forEach(e => e.classList.remove('active'));
+        project.classList.add('active');
+    });
+});
